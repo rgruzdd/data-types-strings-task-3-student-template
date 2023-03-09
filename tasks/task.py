ef9644f7ff0266bdff5011de39b2dc2ec3c9a2d8
Gruzin <gruzd.roman@inbox.ru>
2023-03-09 22:12:06 +0300

    Commit with a solution

--- tasks/task.py
+++ tasks/task.py
@@ -2,4 +2,15 @@ def replacer(s: str) -> str:
     """
     Add your code here
     """
-    return ''
+    new_s = ''
+    for i in s:
+        if i == "'":
+            new_s += '"'
+        elif i == '"':
+            new_s += "'"
+        else:
+            new_s += i
+    return new_s
+
+replacer(' abc"jfjf\'jf ')
+
